497d4d2c079b0c4fca7d2dec2dcde507ebb58764
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-01-16 19:33:08 +0100

    Use percentages for image

--- src/autoreviewer/jcheminf/summarize.py
+++ src/autoreviewer/jcheminf/summarize.py
@@ -5,15 +5,16 @@ import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from matplotlib.ticker import FuncFormatter
 
 
 def _p_status(x):
     if pd.isna(x):
         return "No Repo"
     elif x <= 0:
-        return "Unpackaged"
+        return False
     else:
-        return "Packaged"
+        return True
 
 
 def _license_status(x):
@@ -25,9 +26,20 @@ def _license_status(x):
         return "Present"
 
 
+def _percentage(df, column):
+    series = df.groupby("year")[column].mean()
+    rdf = series.to_frame().reset_index()
+    return rdf
+
+
+def _percentage_axis(ax):
+    formatter = FuncFormatter(lambda y, _: "{:.1%}".format(y))
+    ax.yaxis.set_major_formatter(formatter)
+
+
 def main():
     """Generate summary charts."""
-    fig, axes = plt.subplots(2, 3, figsize=(11, 5), sharey=True)
+    fig, axes = plt.subplots(2, 3, figsize=(11, 5))
 
     axes = axes.ravel()
 
@@ -37,39 +49,61 @@ def main():
     df["date"] = pd.to_datetime(df["date"])
     df = df[df["date"].notna()]
     df["year"] = df["date"].dt.year
-    # 2017 was the first year where a repository was detected
+    # 2017 was the first year, where a repository was detected
     df = df[(2017 < df["year"]) & (df["year"] < today.year)]  # don't make ragged chart
 
-    sns.countplot(x="year", hue="has_github", data=df, ax=axes[0])
-    axes[0].set_ylabel("Count")
+    sns.barplot(x="year", y="has_github", data=_percentage(df, "has_github"), ax=axes[0])
+    axes[0].set_ylabel("")
     axes[0].set_xlabel("")
-    axes[0].set_title("Papers with Code")
+    axes[0].set_title("Has GitHub Repo")
+    _percentage_axis(axes[0])
 
-    sns.countplot(x="year", hue="has_issues", data=df[df["has_github"]], ax=axes[1])
-    axes[1].set_ylabel("Count")
+    sns.barplot(
+        x="year", y="has_issues", data=_percentage(df[df["has_github"]], "has_issues"), ax=axes[1]
+    )
+    axes[1].set_ylabel("")
     axes[1].set_xlabel("")
-    axes[1].set_title("Issue Tracker Availability")
+    axes[1].set_title("Has Issue Tracker")
+    _percentage_axis(axes[1])
 
     df["package_status"] = df["pyroma_score"].map(_p_status)
-    sns.countplot(x="year", hue="package_status", data=df[df["has_github"]], ax=axes[2])
+    sns.barplot(
+        x="year",
+        y="package_status",
+        data=_percentage(df[df["has_github"]], "package_status"),
+        ax=axes[2],
+    )
     axes[2].set_xlabel("")
-    axes[2].set_ylabel("Count")
-    axes[2].set_title("Packaging Status")
+    axes[2].set_ylabel("")
+    axes[2].set_title("Packaged Code")
+    _percentage_axis(axes[2])
 
-    sns.countplot(x="year", hue="is_blackened", data=df[df["has_github"]], ax=axes[3])
+    sns.barplot(
+        x="year",
+        y="is_blackened",
+        data=_percentage(df[df["has_github"]], "is_blackened"),
+        ax=axes[3],
+    )
     axes[3].set_xlabel("")
-    axes[3].set_ylabel("Count")
-    axes[3].set_title("Linting Status")
+    axes[3].set_ylabel("")
+    axes[3].set_title("Applied Linting")
+    _percentage_axis(axes[3])
 
-    sns.countplot(x="year", hue="has_installation_docs", data=df[df["has_github"]], ax=axes[4])
+    sns.barplot(
+        x="year",
+        y="has_installation_docs",
+        data=_percentage(df[df["has_github"]], "has_installation_docs"),
+        ax=axes[4],
+    )
     axes[4].set_xlabel("")
-    axes[4].set_ylabel("Count")
-    axes[4].set_title("Installation Documentation Status")
+    axes[4].set_ylabel("")
+    axes[4].set_title("Has Installation Documentation")
+    _percentage_axis(axes[4])
 
     df["license_status"] = df["license"].map(_license_status)
     sns.countplot(x="year", hue="license_status", data=df[df["has_github"]], ax=axes[5])
     axes[5].set_xlabel("")
-    axes[5].set_ylabel("Count")
+    axes[5].set_ylabel("")
     axes[5].set_title("License Status")
 
     plt.suptitle("Analysis of J. Chem. Inf. Papers", fontsize=16)
